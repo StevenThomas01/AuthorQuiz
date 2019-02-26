ed8f163c275f7e53bb8e61e5f85f852baf0453df
Kiet Lo <klo@intelligentenvironments.comm>
2019-02-26 11:58:26 +0000

    Change filenames to AuthorQuiz

---
+++ src/AuthorQuiz.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import "./bootstrap.min.css";
+
+export default class AuthorQuiz extends Component {
+  render() {
+    return <div>Author Quiz</div>;
+  }
+}
